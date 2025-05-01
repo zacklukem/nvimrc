da2033f07b87572a9cdd31de617b6d1cc9a564c1
Zachary Mayhew <zlmayhew@sep.com>
2025-05-01 10:07:01 -0400

    Clean up config

--- init.lua
+++ init.lua
@@ -62,18 +62,8 @@ require("lazy").setup({
     -- blink.cmp
     {
       'saghen/blink.cmp',
-      -- optional: provides snippets for the snippet source
       dependencies = { 'rafamadriz/friendly-snippets' },
-
-      -- use a release tag to download pre-built binaries
       version = '1.*',
-      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
-      -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
-
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
       opts = {
         -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
         -- 'super-tab' for mappings similar to vscode (tab to accept)
@@ -85,30 +75,12 @@ require("lazy").setup({
         -- C-n/C-p or Up/Down: Select next/previous item
         -- C-e: Hide menu
         -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
         keymap = { preset = 'super-tab' },
-
-        appearance = {
-          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = 'mono'
-        },
-
-        -- (Default) Only show the documentation popup when manually triggered
+        appearance = { nerd_font_variant = 'mono' },
         completion = { documentation = { auto_show = false } },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
           default = { 'lsp', 'path', 'snippets', 'buffer' },
         },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
         fuzzy = { implementation = "prefer_rust_with_warning" }
       },
       opts_extend = { "sources.default" }
@@ -138,12 +110,7 @@ require("lazy").setup({
         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
       },
       init = function() vim.g.barbar_auto_setup = false end,
-      opts = {
-        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-        -- animation = true,
-        -- insert_at_start = true,
-        -- …etc.
-      },
+      opts = {},
       version = '^1.0.0', -- optional: only update when a new 1.x version is released
     },
   },
