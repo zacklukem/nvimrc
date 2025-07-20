0f1c287b79c2a706bd7210fa9cabcd447bdcece7
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2025-07-19 20:16:24 -0400

    Remove copilot and cmp

--- init.lua
+++ init.lua
@@ -62,50 +62,6 @@ require("lazy").setup({
       end,
     },
 
-    -- blink.cmp
-    {
-      'saghen/blink.cmp',
-      dependencies = { 'rafamadriz/friendly-snippets' },
-      version = '1.*',
-      opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        keymap = { preset = 'super-tab' },
-        appearance = { nerd_font_variant = 'normal' },
-        completion = { documentation = { auto_show = false } },
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-      },
-      opts_extend = { "sources.default" }
-    },
-
-    -- copilot.lua
-    {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      event = "InsertEnter",
-      config = function()
-        require("copilot").setup({
-          suggestion = {
-            auto_trigger = true,
-            keymap = {
-              accept = "<Tab>",
-            },
-          },
-        })
-      end,
-    },
-
     -- barbar.nvim
     {
       'romgrk/barbar.nvim',
